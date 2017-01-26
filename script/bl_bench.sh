00dbe19b72347c1dfcc7bbf17b3aaa2fe04e86dd
gatieme <gatieme@163.com>
2017-01-26 19:25:56 +0800

    添加新的脚本...

--- script/bl_bench.sh
+++ script/bl_bench.sh
@@ -2,19 +2,98 @@
 #  perf sched messaging -g $GROUP
 #------------------------------------------
 
+perf_sched_bench_clear()
+{
+        rm -rf $LOG_DIR
+        mkdir -p $LOG_DIR
+        echo -e "\n+++++++++++++++++++++"
+        echo "clear $LOG_DIR"
+        echo -e "+++++++++++++++++++++\n"
+}
+
+perf_sched_bench_run( )
+{
+#  写入表头信息
+echo "#bench = $BENCH"          > $RESULT_FILE
+echo "#group = $MAX_GROUP"      >> $RESULT_FILE
+echo "#num   = $LOOP"            >> $RESULT_FILE
+for (( GROUP = 1; GROUP <= $MAX_GROUP; GROUP ++ ))   #  增加GROUP的值
+do
+        echo "+++++++++++++++++++++"
+        echo "#messaging $GROUP"
+        echo "$BENCH"
+        echo "+++++++++++++++++++++"
+
+        echo "====================="
+        for (( i = 1; i <= $LOOP; i++ ))          #  每次统计循环进行NUM次以防止误差
+        do
+
+                echo -e "GROUP : $GROUP, Test Num : $i"
+                if [ $i == 1 ]; then
+                        echo -e "GROUP : $GROUP, Test Num : $i" > $LOG_DIR/$GROUP.log
+                else
+                        echo -e "GROUP : $GROUP, Test Num : $i" >> $LOG_DIR/$GROUP.log
+                fi
+                if [ $BENCH == "messaging" ]; then
+                        #perf bench sched messaging -g $GROUP
+                        perf bench sched messaging -g $GROUP >> $LOG_DIR/$GROUP.log
+                elif [ $BENCH == "pipe" ]; then
+                        #perf bench sched pipe -l $GROUP
+                        perf bench sched pipe -l $GROUP >> $LOG_DIR/$GROUP.log
+                fi
+        done
+        echo -e "=====================\n\n"
+        #python ./readlog.py -p $POS -t $TYPE -n $LOOP >> $RESULT_DIR/$BENCH.log
+done
+}
+
+
+raw_input()
+{
+        while true;
+        do
+                stty -icanon min 0 time 100
+                echo -n "Use default to run ? (y/n) :"
+                read Arg
+                case $Arg in
+
+                        Y|y|YES|yes)
+                        break;;
+
+                        N|n|NO|no)
+                        exit;;
+
+                        "")  #Autocontinue
+                        break;;
+                esac
+        done
+
+        echo
+}
+
+
+
 RESULT_DIR=RESULT/perf
 
 echo $#
 BENCH="messaging"
 MAX_GROUP=100
-NUM=1
-
+LOOP=1
+LOG_DIR=""
+RESULT_FILE=""
 
 if [ $# == 0 ]; then
-        echo "use default"
+        echo "Usage :"
+        echo -e "\t$0 [messageing|pipe] max_group loop"
+        echo -e "\tmessage      : use perf bench messaging -g max_group"
+        echo -e "\tpipe         : use perf bench pipe -l max_group"
+        echo -e "\tmax_group    : see group@messaging loop@pipe"
+        echo -e "\tloop         : loop to reduces system error "
+        
+        raw_input
         #BENCH="messaging"
         #MAX_GROUP=100
-        #NUM=1
+        #LOOP=1
 elif [ $# == 1 ]; then
         BENCH=$1
         #MAX_GROUP=100
@@ -24,38 +103,19 @@ elif [ $# == 2 ]; then
 elif [ $# == 3 ]; then
         BENCH=$1
         MAX_GROUP=$2
-        NUM=$3
+        LOOP=$3
 fi
 
-echo "bench = $BENCH"
-echo "group = $MAX_GROUP"
-echo "num   = $NUM"
+LOG_DIR=$RESULT_DIR/$BENCH/$MAX_GROUP-$LOOP
+RESULT_FILE=$RESULT_DIR/$BENCH/$BENCH-$MAX_GROUP-$LOOP.log
+
+echo "bench      = $BENCH"
+echo "group      = $MAX_GROUP"
+echo "num        = $LOOP"
+echo "logdir     = $LOG_DIR"
+echo "resultfile = $RESULT_FILE"
 sleep 1
 
-mkdir -p $RESULT_DIR
-mkdir -p $RESULT_DIR/$BENCH
 
-echo "#messaging $GROUP " >> $RESULT_DIR/$BENCH/$BENCH.log
-for (( GROUP = 1; GROUP <= $MAX_GROUP; GROUP ++ ))   #  增加GROUP的值
-do
-        echo "+++++++++++++++++++++"
-        echo "#messaging $GROUP"
-        echo "+++++++++++++++++++++"
-
-        echo "====================="
-        for (( i = 1; i <= $NUM; i++ ))          #  每次统计循环进行NUM次以防止误差
-        do
-
-                echo -e "GROUP : $GROUP, Test Num : $i"
-                echo -e "GROUP : $GROUP, Test Num : $i" > $RESULT_DIR/$BENCH/$GROUP.log
-                if [ $BENCH == "messaging" ]; then
-                        #perf bench sched messaging -g $GROUP
-                        perf bench sched messaging -g $GROUP >> $RESULT_DIR/$BENCH/$GROUP.log
-                elif [ $BENCH == "pipe" ]; then
-                        #perf bench sched pipe -l $GROUP
-                        perf bench sched pipe -l $GROUP >> $RESULT_DIR/$BENCH/$GROUP.log
-                fi
-        done
-        echo -e "=====================\n\n"
-        #python ./readlog.py -p $POS -t $TYPE -n $NUM >> $RESULT_DIR/$BENCH/$BENCH.log
-done
+perf_sched_bench_clear
+perf_sched_bench_run
---
+++ script/readlog.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+
+#!coding:utf-8
+
+import re
+import sys
+import urllib2
+import argparse
+import commands
+import os
+import subprocess
+
+
+
+positions = ""
+types = ""
+numbers = ""
+def readFile(file):
+    """
+    """
+    file_object = open(file)
+
+    try :
+
+        read_data = file_object.read( )
+        return read_data
+
+    finally :
+
+        file_object.close( )
+
+
+
+def writeData(positions, types, nums, sigalrm, sigsegv, sigill) :
+    """
+    """
+    resultfile = "./RESULT/" + nums + "/result.log"
+    file_object = open(resultfile, "a")
+    file_object.write(positions.center(10) + types.center(20) + str(sigalrm).center(10) + str(sigsegv).center(10) + str(sigill).center(10) + "\n")
+    file_object.close( )
+
+
+
+
+
+def getReItem(data) :
+    """
+    """
+    try:
+        #  匹配的信息如下
+        #  Total time: 0.038 [sec]
+        #reStr = r'Total time: ([-+]?[0-9]*\.?[0-9]+) [sec]'
+        reStr = r'\s*Total time: ([0-9]*\.?[0-9]+) [sec]\s*'
+
+        pattern = re.compile(reStr, re.S)
+        myItems = re.findall(pattern, data)
+        #print len(myItems)
+
+        #print myItems
+        for item in myItems     :
+                print item[0]
+
+        return myItems
+    finally:
+        pass
+
+
+def getResult(myItems):
+    for item in myItems :
+        print item
+
+
+
+
+if __name__ == "__main__" :
+    # 测试正则表达式
+
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    if len(sys.argv) > 1:               #  如果在程序运行时，传递了命令行参数
+        pass
+        #  打印传递的命令行参数的信息
+        #print "您输入的所有参数共 %d 个，信息为 sys.argv = %s" % (len(sys.argv), sys.argv)
+
+        #for i, eachArg in enumerate(sys.argv):
+        #    print "[%d] = %s" % (i, eachArg)
+    else:
+        print "Useage : read.py file..."
+        exit(0)
+
+    parser = argparse.ArgumentParser( )
+    parser.add_argument("-u", "--user", dest = "user", help = "Who you want to read...")
+    parser.add_argument("-f", "--file", dest = "resultfile", help = "The file you want to read...")
+    parser.add_argument("-g", "--group", dest = "group", help = "The file you want to read...")
+    parser.add_argument("-l", "--loop", dest = "loop", help = "The file you want to read...")
+    args = parser.parse_args( )
+
+    resultfile = "./RESULT/" + args.user + "/" +args.group + "-" +args.loop + "/" + args.group + ".log"
+
+    #print resultfile
+    resultdata = readFile(resultfile)
+    print resultdata
+    myItems = getReItem(resultdata)
+    print myItems
+    if (len(myItems) != args.loop) :
+        print "miss something in read %s" % (resultfile)
+        exit(0)
+    result = getResult(myItems)
+
+    pass
+
