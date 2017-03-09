33d11b011f8c1d9ef75eb6b047f33e9edf2cd945
gatieme <gatieme@163.com>
2017-03-09 21:23:23 +0800

    测试程序...

--- script/bench/runsplash.sh
+++ script/bench/runsplash.sh
@@ -1,14 +1,14 @@
 #--------------
 #  splash-2
 #--------------
-sh deal.sh little splash barnes         20
-sh deal.sh little splash ocean          20
-sh deal.sh little splash radiosity      20
-sh deal.sh little splash raytrace       20
-sh deal.sh little splash water-nsquared 20
-sh deal.sh little splash water-spatial  20
-sh deal.sh little splash cholesky       20
-sh deal.sh little splash lu             20
-sh deal.sh little splash fft            20
-sh deal.sh little splash radix          20
+sh deal.sh powersave splash barnes         20
+sh deal.sh powersave splash ocean          20
+sh deal.sh powersave splash radiosity      20
+sh deal.sh powersave splash raytrace       20
+sh deal.sh powersave splash water-nsquared 20
+sh deal.sh powersave splash water-spatial  20
+sh deal.sh powersave splash cholesky       20
+sh deal.sh powersave splash lu             20
+sh deal.sh powersave splash fft            20
+sh deal.sh powersave splash radix          20
 
