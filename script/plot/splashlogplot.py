78a274538baa9ac2c029ae8cde0b79e2619a2329
gatieme <gatieme@163.com>
2017-03-10 17:31:05 +0800

    。。。

--- script/plot/splashlogplot.py
+++ script/plot/splashlogplot.py
@@ -320,5 +320,5 @@ if __name__ == "__main__" :
     print "===============================================================\n" 
     
     #ShowPerfPlot(nameTuple, appTuple, plotDataList, colorTuple, standardized = False, minY = 0, maxY = 4200)
-    ShowPerfPlot(nameTuple, appTuple, plotDataList, colorTuple, standardized = True, minY = 0, maxY = 4)
+    ShowPerfPlot(nameTuple, appTuple, plotDataList, colorTuple, standardized = True, minY = 0, maxY = 7)
     exit(0)
